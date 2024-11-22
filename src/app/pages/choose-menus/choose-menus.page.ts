7365a5e637433fef6b55b9a11ac1c1e83cf3b712
Enrique Lopez Garcia <kikeloga75@gmail.com>
2024-11-22 12:29:21 +0100

    choose-menus V2

--- src/app/pages/choose-menus/choose-menus.page.ts
+++ src/app/pages/choose-menus/choose-menus.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonModal, IonButtons } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonModal, IonButtons, IonIcon } from '@ionic/angular/standalone';
 import { Class } from '../../common/models/class.models'; 
 import { MenuType, Menu } from '../../common/models/menu.models'; 
 import { Timestamp } from 'firebase/firestore';
@@ -14,28 +14,16 @@ import { MenuService } from '../../common/services/menu.service';
   templateUrl: './choose-menus.page.html',
   styleUrls: ['./choose-menus.page.scss'],
   standalone: true,
-  imports: [IonButtons,  IonModal, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonList, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonButtons,  IonModal, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonList, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ChooseMenusPage implements OnInit  {
-  @ViewChild(IonModal) modal: IonModal;
+  // IMPORTANTE - Sería interesante añadir el estado de la clase con el número 
+  // de integrantes de cada una para así controlar cuando están completas y que 
+  // no se pueda añadir más menús que alumnos.
   
   classes: Class[] = [];
   menuTypes: MenuType[] = [];
   selectedClass: Class | null = null;
-  // IMPORTANTE - Sería interesante añadir el estado de la clase con el número 
-  // de integrantes de cada una para así controlar cuando están completas y que 
-  // no se pueda añadir más menús que alumnos.
-
-  // classes: Class[] = [
-  //   { id: '1', name: 'Class 1', pictogramId: 'pictogram1' },
-  //   { id: '2', name: 'Class 2', pictogramId: 'pictogram2' },
-  //   { id: '3', name: 'Class 3', pictogramId: 'pictogram3' }
-  // ];
-  // menuTypes: MenuType[] = [
-  //   { id: '1', name: 'Menu Type 1', pictogramId: 'pictogram1', visible: true },
-  //   { id: '2', name: 'Menu Type 2', pictogramId: 'pictogram2', visible: true },
-  //   { id: '3', name: 'Menu Type 3', pictogramId: 'pictogram3', visible: true }
-  // ];
   menu: Menu = {
     id: '',
     date: Timestamp.fromDate(new Date(new Date().setHours(0, 0, 0, 0))),
@@ -84,9 +72,5 @@ export class ChooseMenusPage implements OnInit  {
     if (this.selectedClass && this.menu.menus[this.selectedClass.name][menuTypeName] > 0) {
       this.menu.menus[this.selectedClass.name][menuTypeName]--;
     }
-  }
-
-  closeModal() {
-    this.modal.dismiss();
   }
 }
